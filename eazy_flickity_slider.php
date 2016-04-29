90a08c1d1c1166523fdf5f3e3f23dc47d200b0b4
Rob Scott <rob@robjscott.com>
2016-04-29 14:12:59 -0500

    Initial Comit
    Add plugin shell (/resources, /resources/css, resources/js)
    add icon for menu,
    add plugin functions (eazy_flickity_slider_functions)

---
+++ eazy_flickity_slider.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+Plugin Name: Eazy Flickity Slider
+Plugin URI: http://robjscott.com/wordpress/
+Description:  Adds slide custom post type for uploading slides. Slides can be added to slider using shortcodes. 
+Version: 1.1.0
+Author: Rob Scott, LLC
+Author URI: http://robjscott.com
+Text Domain: ez-flickity-slider
+License: GPLv2 or any later version
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+  die;
+}
+
+//define plugin path
+define( 'EZ_FLICKITY_ELEMENTS_PATH', plugin_dir_path(__FILE__) );
+define( 'EZ_FLICKITY_ELEMENTS_FILE_PATH', __FILE__ );
+define( 'EZ_FLICKITY_ELEMENTS_URL', str_replace('index.php','',plugins_url( 'index.php', __FILE__ )));
+
+//add functions
+require_once(EZ_FLICKITY_ELEMENTS_PATH . 'resources/eazy_flickity_slider_functions.php');
